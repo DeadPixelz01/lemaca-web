f7f4c0644174d4abb683ea0cb771c69d5db67a01
DeadPixelz01 <deadpixelz01@gmail.com>
2023-05-25 14:42:58 +1000

    adding information

--- src/Home.js
+++ src/Home.js
@@ -12,7 +12,7 @@ const Home = () => {
                         <div className="col-md-10 col-xl-8 text-center d-flex d-sm-flex d-md-flex justify-content-center align-items-center mx-auto justify-content-md-start align-items-md-center justify-content-xl-center" style={{backgroundColor: 'rgba(33,32,32,0.66)', borderRadius: '6px'}}>
                             <div>
                                 <h1 className="text-uppercase fw-bold mb-3" style={{color: 'rgb(255,255,255)'}}>Welcome to Lemaca's alpacas</h1>
-                                <p className="mb-4" style={{color: 'rgb(255,255,255)'}}>A boutique alpaca farm residing in central Victroria specialising in dark&nbsp;Huacayas</p>
+                                <p className="mb-4" style={{color: 'rgb(255,255,255)'}}>A boutique alpaca farm residing in central Victroria specialising in &nbsp;Huacayas</p>
                             </div>
                         </div>
                     </div>
@@ -29,7 +29,7 @@ const Home = () => {
                                     <path d="M5.255 5.786a.237.237 0 0 0 .241.247h.825c.138 0 .248-.113.266-.25.09-.656.54-1.134 1.342-1.134.686 0 1.314.343 1.314 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.003.217a.25.25 0 0 0 .25.246h.811a.25.25 0 0 0 .25-.25v-.105c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.267 0-2.655.59-2.75 2.286zm1.557 5.763c0 .533.425.927 1.01.927.609 0 1.028-.394 1.028-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94z"></path>
                                 </svg></div>
                             <h4 className="card-title">About us</h4>
-                            <p className="card-text">Erat netus est hendrerit, nullam et quis ad cras porttitor iaculis. Bibendum vulputate cras aenean.</p>
+                            <p className="card-text">s</p>
                         </div>
                     </div>
                 </div>
