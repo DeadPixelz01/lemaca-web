f0b9c9f0593f33f7bdd2ce90435c5509ae4c4e8c
DeadPixelz01 <deadpixelz01@gmail.com>
2025-08-25 19:33:00 +1000

    Update MainFooter.js

--- src/MainFooter.js
+++ src/MainFooter.js
@@ -8,7 +8,7 @@ function MainFooter() {
                                 <li className="list-inline-item"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16" className="bi bi-envelope-fill">
                                         <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555ZM0 4.697v7.104l5.803-3.558L0 4.697ZM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757Zm3.436-.586L16 11.801V4.697l-5.803 3.546Z"></path>
                                     </svg></li>
-                                <li className="list-inline-item"><a href="mailto:<loumal@optusnet.com.au>">loumal@optusnet.com.au</a></li>
+                                <li className="list-inline-item"><a href="mailto:<lemaca.alpaca@gmail.com>">lemaca.alpaca@gmail.com</a></li>
                             </ul>
                         </div>
                         <div className="col">
